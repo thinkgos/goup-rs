31d70fc79bdc0436cff368afd0bfcda77f42721f
thinkgos <thinkgo@aliyun.com>
2024-01-17 16:01:38 +0800

    fix: add toolchain more option

--- goup-downloader/src/lib.rs
+++ goup-downloader/src/lib.rs
@@ -1,4 +1,30 @@
 mod archived;
 mod downloader;
 
+use std::str::FromStr;
+
 pub use downloader::Downloader;
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub enum Toolchain {
+    Stable,
+    Nightly,
+    Beta,
+    Semver(String),
+}
+
+impl FromStr for Toolchain {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(match s {
+            "stable" => Self::Stable,
+            "nightly" | "tip" | "gotip" => Self::Nightly,
+            "beta" => Self::Beta,
+            _ => {
+                // TODO: version validation
+                Self::Semver(s.to_owned())
+            }
+        })
+    }
+}
--- goup/src/command/install.rs
+++ goup/src/command/install.rs
@@ -2,6 +2,7 @@ use clap::Args;
 
 use goup_consts::consts;
 use goup_downloader::Downloader;
+use goup_downloader::Toolchain;
 use goup_version::Version;
 
 use super::Run;
@@ -9,8 +10,9 @@ use super::Run;
 #[derive(Args, Debug, PartialEq)]
 #[command(disable_version_flag = true)]
 pub struct Install {
-    /// target go version
-    version: Option<String>,
+    /// toolchain name, such as 'stable', 'nightly'('tip', 'gotip',), beta, '1.21.4' or 'go1.21.4'
+    #[arg(default_value = "stable")]
+    toolchain: String,
     /// an optional change list (CL), If the version is 'tip'
     cl: Option<String>,
     /// host that is used to download Go.
@@ -23,17 +25,26 @@ pub struct Install {
 
 impl Run for Install {
     fn run(&self) -> Result<(), anyhow::Error> {
-        let version = if let Some(s) = self.version.as_deref() {
-            Version::normalize(s)
-        } else {
-            Version::get_upstream_latest_go_version(&self.host)?
+        let toolchain = self.toolchain.parse()?;
+
+        let version = match toolchain {
+            Toolchain::Nightly => {
+                println!("Installing gotip ...");
+                Downloader::install_go_tip(self.cl.as_deref())?;
+                "gotip".to_owned()
+            }
+            _ => {
+                // TODO: beta stable semver
+                let version = if let Toolchain::Semver(s) = toolchain {
+                    Version::normalize(&s)
+                } else {
+                    Version::get_upstream_latest_go_version(&self.host)?
+                };
+                println!("Installing {} ...", version);
+                Downloader::install_go_version(&version)?;
+                version
+            }
         };
-        println!("Installing {} ...", version);
-        if version == "gotip" {
-            Downloader::install_go_tip(self.cl.as_deref())?;
-        } else {
-            Downloader::install_go_version(&version)?;
-        }
         if !self.dry {
             Version::set_go_version(&version)?;
         }
