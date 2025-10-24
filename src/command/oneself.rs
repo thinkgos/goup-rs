a4d71d41bf1e1603db08c28af87955834c00b125
0xk1f0 <dev@k1f0.dev>
2025-10-24 22:40:39 +0200

    feat(command/oneself): dont update or uninstall when compiled with no-self-update
    Signed-off-by: 0xk1f0 <dev@k1f0.dev>

--- src/command/oneself.rs
+++ src/command/oneself.rs
@@ -40,6 +40,11 @@ impl Run for Oneself {
     fn run(&self) -> Result<(), anyhow::Error> {
         match self.command {
             Command::Update => {
+                if cfg!(feature = "no-self-update") {
+                    log::warn!("self-update is disabled for this build of goup");
+                    log::warn!("you should use your system package manager to update goup");
+                    return Ok(());
+                }
                 let cmd = Cli::command();
                 let status = Update::configure()
                     .repo_owner("thinkgos")
@@ -53,6 +58,11 @@ impl Run for Oneself {
                 log::info!("Update status: `v{}`!", status.version());
             }
             Command::Uninstall(ref arg) => {
+                if cfg!(feature = "no-self-update") {
+                    log::warn!("self-uninstall is disabled for this build of goup");
+                    log::warn!("you should use your system package manager to uninstall goup");
+                    return Ok(());
+                }
                 let confirmation = arg.no_confirm
                     || Confirm::with_theme(&ColorfulTheme::default())
                         .with_prompt("Do you want to uninstall goup?")
