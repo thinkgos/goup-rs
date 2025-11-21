a0f4b6c4265ea3ab179cae0ca11537f92583fa52
thinkgos <thinkgo@aliyun.com>
2025-11-21 10:34:39 +0800

    removed: remove prettytable-rs dep

--- src/command/env.rs
+++ src/command/env.rs
@@ -1,5 +1,6 @@
+use std::io::{self, Write};
+
 use clap::Args;
-use prettytable::{Table, row};
 
 use super::Run;
 use crate::{consts, dir::Dir};
@@ -9,40 +10,62 @@ pub struct Env;
 
 impl Run for Env {
     fn run(&self) -> Result<(), anyhow::Error> {
-        let mut table = Table::new();
+        let envs = [
+            ("Key", "Value".to_owned(), "Explain"),
+            (
+                consts::GOUP_HOME,
+                Dir::goup_home()
+                    .unwrap_or_default()
+                    .to_string_lossy()
+                    .to_string(),
+                "Get goup home directory, default: '$HOME/.goup'",
+            ),
+            (
+                consts::GOUP_GO_VERSION,
+                consts::go_version().unwrap_or("current".to_owned()),
+                "Shell session target go version, default: 'current'",
+            ),
+            (
+                consts::GOUP_GO_REGISTRY_INDEX,
+                consts::go_registry_index(),
+                "Registry index of go version",
+            ),
+            (
+                consts::GOUP_GO_REGISTRY,
+                consts::go_registry(),
+                "Registry of go archive file",
+            ),
+            (
+                consts::GOUP_GO_SOURCE_GIT_URL,
+                consts::go_source_git_url(),
+                "Source git url, use by tip|nightly or index of go version",
+            ),
+            (
+                consts::GOUP_GO_SOURCE_GIT_URL,
+                consts::go_source_upstream_git_url(),
+                "Source upstream git url, use by tip|nightly",
+            ),
+        ];
 
-        table.add_row(row!["Key", "Value", "Explain"]);
-        table.add_row(row![
-            consts::GOUP_HOME,
-            Dir::goup_home().unwrap_or_default().to_string_lossy(),
-            "Get goup home directory, default: '$HOME/.goup'",
-        ]);
-        table.add_row(row![
-            consts::GOUP_GO_VERSION,
-            consts::go_version().unwrap_or("current".to_owned()),
-            "Shell session target go version, default: 'current'",
-        ]);
-        table.add_row(row![
-            consts::GOUP_GO_REGISTRY_INDEX,
-            consts::go_registry_index(),
-            "Registry index of go version",
-        ]);
-        table.add_row(row![
-            consts::GOUP_GO_REGISTRY,
-            consts::go_registry(),
-            "Registry of go archive file",
-        ]);
-        table.add_row(row![
-            consts::GOUP_GO_SOURCE_GIT_URL,
-            consts::go_source_git_url(),
-            "Source git url, use by tip|nightly or index of go version",
-        ]);
-        table.add_row(row![
-            consts::GOUP_GO_SOURCE_GIT_URL,
-            consts::go_source_upstream_git_url(),
-            "Source upstream git url, use by tip|nightly",
-        ]);
-        table.printstd();
+        let mut stdout = io::stdout().lock();
+        let write_separator = |write: &mut dyn Write| {
+            writeln!(
+                write,
+                "{}-+-{}-+-{}-|",
+                "-".repeat(22),
+                "-".repeat(30),
+                "-".repeat(60)
+            )
+        };
+        write_separator(&mut stdout)?;
+        for (i, (k, v, e)) in envs.iter().enumerate() {
+            writeln!(stdout, "{:<22} | {:<30} | {:<60} |", k, v, e)?;
+            if i == 0 {
+                write_separator(&mut stdout)?;
+            }
+        }
+        write_separator(&mut stdout)?;
+        stdout.flush()?;
         Ok(())
     }
 }
