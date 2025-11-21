f7348518750cf8b2a30e7e6e33320ce78dcd8337
thinkgos <thinkgo@aliyun.com>
2025-11-21 09:54:09 +0800

    feat: add ls-remote mark installed version

--- src/command/search.rs
+++ src/command/search.rs
@@ -1,7 +1,10 @@
+use std::collections::HashSet;
+
 use clap::Args;
+use owo_colors::OwoColorize;
 
 use super::Run;
-use crate::{consts, registry::RegistryIndex};
+use crate::{consts, registry::RegistryIndex, version::Version};
 
 #[derive(Args, Debug, PartialEq)]
 pub struct Search {
@@ -15,13 +18,17 @@ pub struct Search {
 impl Run for Search {
     fn run(&self) -> Result<(), anyhow::Error> {
         let filter = self.filter.as_ref().and_then(|s| s.parse().ok());
-        let registry_index = RegistryIndex::new(&self.registry_index);
-        registry_index
-            .list_upstream_go_versions_filter(filter)?
-            .iter()
-            .for_each(|v| {
-                println!("{v}");
-            });
+        let remote_versions =
+            RegistryIndex::new(&self.registry_index).list_upstream_go_versions_filter(filter)?;
+        let local_versions = Version::list_go_version().unwrap_or_default();
+        let local_versions: HashSet<_> = local_versions.iter().map(|v| &v.version).collect();
+        remote_versions.iter().for_each(|version| {
+            if local_versions.contains(&version) {
+                println!("{}{}", version.yellow(), "(installed)".yellow());
+            } else {
+                println!("{version}");
+            }
+        });
         Ok(())
     }
 }
